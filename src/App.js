886bfc2423a2ef38d1409612dc3f85a380126149
jamhea23 <83547828+jamhea23@users.noreply.github.com>
2023-01-19 23:50:28 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      { name: "Fundamentals of React", exercises: 10 },
 
-function App() {
+      { name: "Using props to pass data", exercises: 7 },
+
+      { name: "State of a component", exercises: 14 },
+    ],
+  };
+
+  const Header = (props) => {
+    console.log(props);
+    return <h1>{props.course}</h1>;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
-}
+};
 
 export default App;
